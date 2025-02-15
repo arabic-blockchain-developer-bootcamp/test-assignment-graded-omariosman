e293fd8c754cacb6bf0ab3383583fae3157eaded
Omar Osman <omarosman23@aucegypt.edu>
2025-02-15 04:46:27 +0200

    Update Assignment1.t.sol

--- test/Assignment1.t.sol
+++ test/Assignment1.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
-
 contract Assignment1Test is Test {
     address public walletAddress;
 
